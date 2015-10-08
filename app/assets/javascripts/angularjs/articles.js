7d2b022c8ad8ca1a24ae1ab13002090a50645507
jamesfwz <james@futureworkz.com>
2015-10-08 13:51:51 +0700

    search + order

--- app/assets/javascripts/angularjs/articles.js
+++ app/assets/javascripts/angularjs/articles.js
@@ -2,11 +2,27 @@ var demoApp = angular.module('demoApp', []);
 
 demoApp.controller('ArticlesIndexCtrl', function ($scope) {
   $scope.articles = [
-    {'title': 'Nexus S',
-     'content': 'Fast just got faster with Nexus S.'},
-    {'title': 'Motorola XOOM™ with Wi-Fi',
-     'content': 'The Next, Next Generation tablet.'},
-    {'title': 'MOTOROLA XOOM™',
-     'content': 'The Next, Next Generation tablet.'}
+    {
+      'title': 'Nexus S',
+      'content': 'Fast just got faster with Nexus S.',
+      'created_at': '2015-06-07'
+    },
+    {
+      'title': 'Motorola XOOM™ with Wi-Fi',
+      'content': 'The Next, Next Generation tablet.',
+      'created_at': '2011-08-02'
+    },
+    {
+      'title': 'Motorola XOOM™ with Wi-Fi',
+      'content': 'The Next, Next Generation tablet.',
+      'created_at': '2016-07-04'
+    },
+    {
+      'title': 'MOTOROLA XOOM™',
+      'content': 'The Next, Next Generation tablet.',
+      'created_at': '2013-01-11'
+    }
   ];
+
+  $scope.orderProp = 'created_at';
 });
