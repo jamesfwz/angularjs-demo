4694c532f9b93e77651b3009fb908d6f74dc67af
jamesfwz <james@futureworkz.com>
2015-10-08 14:20:36 +0700

    get data from controller api

--- app/assets/javascripts/angularjs/articles.js
+++ app/assets/javascripts/angularjs/articles.js
@@ -1,28 +1,9 @@
 var demoApp = angular.module('demoApp', []);
 
-demoApp.controller('ArticlesIndexCtrl', function ($scope) {
-  $scope.articles = [
-    {
-      'title': 'Nexus S',
-      'content': 'Fast just got faster with Nexus S.',
-      'created_at': '2015-06-07'
-    },
-    {
-      'title': 'Motorola XOOM™ with Wi-Fi',
-      'content': 'The Next, Next Generation tablet.',
-      'created_at': '2011-08-02'
-    },
-    {
-      'title': 'Motorola XOOM™ with Wi-Fi',
-      'content': 'The Next, Next Generation tablet.',
-      'created_at': '2016-07-04'
-    },
-    {
-      'title': 'MOTOROLA XOOM™',
-      'content': 'The Next, Next Generation tablet.',
-      'created_at': '2013-01-11'
-    }
-  ];
+demoApp.controller('ArticlesIndexCtrl', function ($scope, $http) {
+  $http.get('articles.json').success(function(data) {
+    $scope.articles = data;
+  });
 
   $scope.orderProp = 'created_at';
 });
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   def index
-    
+    respond_to do |format|
+      format.html
+      format.json { render :json => Article.all }
+    end
   end
 end
